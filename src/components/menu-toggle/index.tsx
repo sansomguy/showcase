b7a7f92f51c82761d609d8312b2ff73dc539547d
Josh Sansom-Sherwill <joshwebd@gmail.com>
2024-06-30 15:23:59 +1000

    Got the appear / disappear logic for the side menu working pretty well

---
+++ src/components/menu-toggle/index.tsx
@@ -0,0 +1,49 @@
+import { $, component$, useOnDocument, useSignal } from "@builder.io/qwik";
+
+import MenuSVG from "~/media/menu.svg?jsx";
+import styles from "./menu-toggle.module.css";
+
+function initTransformOrigin(button: Element) {
+  const lines = button.getElementsByClassName(
+    "menu_svg__line"
+  ) as HTMLCollectionOf<SVGRectElement>;
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    // calculate the center of the line
+    const bbox = line.getBBox();
+    const centerX = bbox.x + bbox.width / 2;
+    const centerY = bbox.y + bbox.height / 2;
+    line.style.transformOrigin = `${centerX}px ${centerY}px`;
+  }
+}
+
+type Props = {
+  open: boolean;
+  onClick: () => void;
+  disappearOnOpen?: boolean;
+  disappearOnClose?: boolean;
+};
+export default component$(({ open, onClick, disappearOnClose, disappearOnOpen }: Props) => {
+  const menuButtonRef = useSignal<Element>();
+  useOnDocument(
+    "DOMContentLoaded",
+    $(() => menuButtonRef.value && initTransformOrigin(menuButtonRef.value))
+  );
+
+  const shouldDisappear = open && disappearOnOpen || !open && disappearOnClose;
+
+  return (
+    <div class={styles.mobileNav}>
+      <span
+        ref={menuButtonRef}
+        class={`${styles.mobileNavButton} ${open ? styles["mobileNavButton--open"] : ''} ${shouldDisappear ? styles["mobileNavButton--disappear"] : ''}`}
+        onClick$={() => {
+        
+          onClick();
+        }}
+      >
+        <MenuSVG />
+      </span>
+    </div>
+  );
+});
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,14 @@
-import { $, Slot, component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+import {
+  $,
+  Slot,
+  component$,
+  useSignal,
+} from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
 import styles from "./layout.module.css";
-import MenuSVG from "~/media/menu.svg?jsx";
 
 import Navigation from "~/components/navigation";
+import MenuToggle from "~/components/menu-toggle";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -16,55 +21,12 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
+
 export default component$(() => {
   const showMobileSideNav = useSignal(() => false);
-
-  useVisibleTask$(() => {
-    function setTransformOrigins() {
-      const [menuButton] = document.getElementsByClassName(
-        styles.mobileNavButton
-      );
-      const lines = menuButton.getElementsByClassName(
-        "menu_svg__line"
-      ) as HTMLCollectionOf<SVGRectElement>;
-      for (let i = 0; i < lines.length; i++) {
-        const line = lines[i];
-        // calculate the center of the line
-        const bbox = line.getBBox();
-        const centerX = bbox.x + bbox.width / 2;
-        const centerY = bbox.y + bbox.height / 2;
-        line.style.transformOrigin = `${centerX}px ${centerY}px`;
-      }
-    }
-    setTransformOrigins();
-  });
-
-  const toggleMobileNav = $(() => {
-    const [menuButton] = document.getElementsByClassName(
-      styles.mobileNavButton
-    );
-
-    function playAnimation() {
-      menuButton.classList.add(styles["mobileNavButton--open"]);
-    }
-
-    function reverseAnimation() {
-      menuButton.classList.remove(styles["mobileNavButton--open"]);
-    }
-
-    
-
-    const open = !showMobileSideNav.value;
-    if (open) {
-      playAnimation();
-    }
-
-    if (!open) {
-      reverseAnimation();
-    }
-
-    showMobileSideNav.value = !!open;
-  });
+  const toggleShowMobileSideNav = $(() => {
+    showMobileSideNav.value = !showMobileSideNav.value;
+  })
 
   return (
     <div
@@ -72,27 +34,11 @@ export default component$(() => {
     >
       <header class={`${styles.navigation}`}>
         <div class={styles["navigation__inner"]}>
-          <div
-            class={styles["navigation__close"]}
-            onClick$={() => {
-              toggleMobileNav();
-            }}
-          >
-            x
-          </div>
+          <MenuToggle open={showMobileSideNav.value} onClick={toggleShowMobileSideNav} disappearOnClose />
           <Navigation />
         </div>
       </header>
-      <div class={styles.mobileNav}>
-        <span
-          class={styles.mobileNavButton}
-          onClick$={(event) => {
-            toggleMobileNav();
-          }}
-        >
-          <MenuSVG />
-        </span>
-      </div>
+      <MenuToggle open={showMobileSideNav.value} onClick={toggleShowMobileSideNav} disappearOnOpen />
       <main class={styles.main}>
         <Slot />
       </main>
