5e769a75a7e92eee133c246d3f132f178dd0d570
Josh Sansom-Sherwill <joshwebd@gmail.com>
2024-07-12 07:15:15 +1000

    Added svg for the about page.

--- src/components/page-icon/index.tsx
+++ src/components/page-icon/index.tsx
@@ -1,5 +1,6 @@
 import WorkerSVG from "~/media/worker.svg?jsx";
 import BrainSVG from "~/media/brain.svg?jsx";
+import MeSVG from '~/media/me.svg?jsx';
 import { component$ } from "@builder.io/qwik";
 
 type IconProps = {
@@ -8,6 +9,8 @@ type IconProps = {
 
 export default component$(({ title }: IconProps) => {
   switch (title) {
+    case "About":
+        return <MeSVG />;
     case "Projects":
       return <WorkerSVG />;
     case "Thoughts":
