457a52147b9a5ab9f2cf099fea0c38c21d357109
Josh Sansom-Sherwill <joshwebd@gmail.com>
2024-07-11 20:35:56 +1000

    fixed up the titles so that they animation on each new navigation

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, useSignal, useStyles$, useTask$, useVisibleTask$ } from "@builder.io/qwik";
 import styles from "./style.css?inline";
 
 type Props = {
@@ -6,19 +6,37 @@ type Props = {
 };
 
 export default component$(({ title }: Props) => {
-  useStylesScoped$(styles);
+  const runAnimation = useSignal(() => false)
+  useStyles$(styles);
+
+  useVisibleTask$(async ({track}) => {
+    track(() => title)
+    
+    runAnimation.value = false;
+    const timeout = setTimeout(() => {
+      runAnimation.value = true;
+    }, 0)
+
+    return () => {clearTimeout(timeout)}
+  })
 
   return (
-    <h1 class="title-popper-base title-popper">
-      {title.split("").map((letter, index) => {
-        return (
-          <div class="hover" key={index}>
-            <span style={`--initial-delay: ${index * 0.2 + 0.5}s;`} key={index}>
-              {letter}
-            </span>
-          </div>
-        );
-      })}
-    </h1>
+    
+      <h1 class={`title-popper-base title-popper`}>
+        {runAnimation.value ? '' : title}
+        {runAnimation.value ? title.split("").map((letter, index) => {
+          return (
+            <div class="hover" key={index}>
+              <span
+                style={`--initial-delay: ${(index * 0.2).toFixed(2)}s;`}
+                key={index} 
+                class={`title-popper__letter ${runAnimation.value ? 'title-popper__run-animation' : ''}`}
+              >
+                {letter}
+              </span>
+            </div>
+          );
+        }) : ''}
+      </h1>
   );
 });
