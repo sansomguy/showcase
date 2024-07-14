0686f5e831eae0d59c385089da0d0c54ba064455
Josh Sansom-Sherwill <joshwebd@gmail.com>
2024-07-14 18:24:09 +1000

    added a favicon and improved the me.svg to have more defined colours and borders

--- src/root.tsx
+++ src/root.tsx
@@ -25,6 +25,7 @@ export default component$(() => {
         <meta charSet={"utf-8"} />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.svg" />
 
         <RouterHead />
         <ServiceWorkerRegister />
