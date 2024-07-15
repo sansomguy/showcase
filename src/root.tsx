dd8d5c2705ba8f9031bc3d641b8df7579b08cd33
Josh Sansom-Sherwill <joshwebd@gmail.com>
2024-07-16 06:24:55 +1000

    Fixed page title width issues

--- src/root.tsx
+++ src/root.tsx
@@ -24,7 +24,7 @@ export default component$(() => {
       <head>
         <meta charSet={"utf-8"} />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <link rel="icon" href="/favicon.svg" />
 
         <RouterHead />
