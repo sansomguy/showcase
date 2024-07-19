e87d956889e8b1c16907845b59f48ed31db7e0a7
Josh Sansom-Sherwill <joshwebd@gmail.com>
2024-07-19 22:44:41 +1000

    Updated the Works page

--- src/routes/work/layout.tsx
+++ src/routes/work/layout.tsx
@@ -1,6 +1,7 @@
 import { Slot, component$ } from "@builder.io/qwik";
 import { useContent } from "@builder.io/qwik-city";
 import { NavLink } from "~/components/nav-link";
+import SideBarLayout from "~/components/side-bar-layout";
 
 export default component$(() => {
   const menu = useContent();
@@ -10,28 +11,32 @@ export default component$(() => {
   );
 
   return (
-    <>
-      <aside>
-        <ul>
-        {projectsMenuItems?.items?.map((item) => {
-          return (
-            <li key={item.text}>
-              <NavLink href="/work">{item.text}</NavLink>
-              <ul>
-                {item.items?.map((subItem) => {
-                  return (
-                    <li key={subItem.text}>
-                      <NavLink href={subItem.href}>{subItem.text}</NavLink>
-                    </li>
-                  );
-                })}
-              </ul>
-            </li>
-          );
-        })}
-        </ul>
-      </aside>
-      <Slot />  
-    </>
+    <SideBarLayout>
+      <div q:slot="side-bar">
+        <div class="notice" style={{ marginTop: 0 }}>
+          <ul>
+            {projectsMenuItems?.items?.map((item) => {
+              return (
+                <li key={item.text}>
+                  <NavLink href="/work">{item.text}</NavLink>
+                  <ul>
+                    {item.items?.map((subItem) => {
+                      return (
+                        <li key={subItem.text}>
+                          <NavLink href={subItem.href}>{subItem.text}</NavLink>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div q:slot="main">
+        <Slot />
+      </div>
+    </SideBarLayout>
   );
 });
