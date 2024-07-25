fd3162d945f54384d056535bd782c087765ffaff
Josh Sansom-Sherwill <joshwebd@gmail.com>
2024-07-25 22:32:21 +1000

    Got it mostly there, however, I'm thinking that I should have things animating without requiring a scroll, then have the scroll come in to add perspective to images and other colourful thigns.

--- src/components/flying-squares/index.tsx
+++ src/components/flying-squares/index.tsx
@@ -5,6 +5,7 @@ import {
   useOnDocument,
   $,
   useOnWindow,
+  useSignal,
 } from "@builder.io/qwik";
 import styles from "./style.css?inline";
 
@@ -14,98 +15,63 @@ import styles from "./style.css?inline";
 // should all move with an arc motion instead of a straight line
 // should all move in the same direction but at different speeds based on starting z-index
 // should have different z-indexes with ones above z-index 0 using background blur effect
-type SquareState = {
-  id: string;
-  x: number;
-  y: number;
-  z: number;
-  opacity: number;
-  initial?: {
-    x: number;
-    y: number;
-    z: number;
-    opacity: number;
-  };
-  ref: Element;
-};
 
 export default component$(() => {
   useStyles$(styles);
   const squares = useStore(createInitialState());
-
-  // useOnWindow(
-  //   "resize",
-  //   $(() => {
-  //     for (const squareKey in squares) {
-  //       const square = squares[squareKey];
-  //       if (square.ref) {
-  //         const htmlTarget = square.ref as HTMLElement;
-  //         const rect = htmlTarget.getBoundingClientRect();
-  //         square.initial = {
-  //           x: rect.right,
-  //           y: rect.top,
-  //         };
-  //       }
-  //     }
-  //   })
-  // );
+  const previousScroll = useSignal(0);
 
   useOnDocument(
     "scroll",
     $((e) => {
       const document = e?.target as Document;
       const window = document.defaultView!;
-      const scrollTop = window.scrollY || document.documentElement.scrollTop;
       const scrollHeight =
         document.documentElement.scrollHeight - window.innerHeight;
-      const progress = scrollTop / scrollHeight;
 
-      const controlPoint = {
-        x: window.innerWidth / 4,
-        y: window.innerWidth / 4,
-        z: 100,
-      } as const;
+      const newScrollTop = window.scrollY || document.documentElement.scrollTop;
+      const scrollDiff = Math.abs(previousScroll.value - newScrollTop);
+      previousScroll.value = newScrollTop;
+      const normalizedDiff = scrollDiff / scrollHeight;
 
-
-      console.log(progress);
-
-      for (const squareKey in squares) {
-        const square = squares[squareKey];
-        if (!square.initial && square.ref) {
-          const htmlTarget = square.ref as HTMLElement;
-          const rect = htmlTarget.getBoundingClientRect();
-
-          square.initial = {
-            x: rect.x,
-            y: rect.y,
-            z: 0,
-            opacity: parseFloat(htmlTarget.style.opacity),
-          };
-        }
-
-        if (square.initial) {
-          const startingPoint = square.initial;
-          const topRight = {
-            x: window.innerWidth,
-            y: window.innerHeight,
-            z: -500,
-          };
-
-          
-
-          const newXY = threePointBezier(
-            startingPoint,
-            controlPoint,
-            topRight,
-            progress
+      
+      requestAnimationFrame(() => {
+        for (const squareKey in squares) {
+          const square = squares[squareKey];
+          if (square.progress >= 1) {
+            resetSquare(square);
+          }
+          square.progress = Math.min(
+            square.progress + square.progressRate * normalizedDiff,
+            1
           );
+          const controlPoint = {
+            x: 50 + square.initial.x,
+            y: 50 - square.initial.y,
+          } as const;
+          const topRight = {
+            x: 100 + square.initial.x,
+            y: 0 - square.initial.y,
+            z: 0,
+          };
 
-          square.x = newXY.x;
-          square.y = newXY.y;
-          square.z = newXY.z;
-          // square.opacity = 0.8 - (square.initial.opacity * progress);
+          square.x = bezier(
+            square.initial.x,
+            controlPoint.x,
+            topRight.x,
+            square.progress
+          );
+          square.y = bezier(
+            square.initial.y,
+            controlPoint.y,
+            topRight.y,
+            square.progress
+          );
+          square.z =
+            ((1 - square.progress) * square.initial.z) + (topRight.z * square.progress);
+          square.opacity = square.progress;
         }
-      }
+      });
     })
   );
 
@@ -115,14 +81,11 @@ export default component$(() => {
         return (
           <div
             key={`square-${square.id}`}
-            class={`flying-square flying-square--${square.id}`}
-            ref={(el) => (square.ref = el)}
-            data-initial-x={square.initial?.x}
-            data-initial-y={square.initial?.y}
+            class={`flying-square`}
             style={{
-              top: `${square.y}px`,
-              left: `${square.x}px`,
-              transform: `perspective(500px) translateZ(${square.z}px)`,
+              left: `${square.initial.x}vw`,
+              top: `${square.initial.y}vh`,
+              transform: `perspective(500px) translate(${square.x}vw, ${square.y}vh) scale(${1 - square.progress})`,
               opacity: square.opacity,
             }}
           ></div>
@@ -132,38 +95,55 @@ export default component$(() => {
   );
 });
 
+type Square = {
+  id: string;
+  x: number;
+  y: number;
+  z: number;
+  initial: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  opacity: number;
+  progress: number;
+  progressRate: number;
+  sum: number;
+};
+
 function createInitialState() {
-  return Array.from({ length: 6 }, (_, i) => i)
-    .map((i) => ({
-      id: `${i + 1}`,
-      x: 0,
-      y: 0,
-      z: 0,
-      ref: null as any,
-      opacity: 1,
-    }))
+  return Array.from({ length: 30 }, (_, i) => i)
+    .map(
+      (i) =>
+        ({
+          id: i.toString(),
+          initial: {}
+        }) as any
+    )
+    .map(resetSquare)
     .reduce(
       (acc, square) => {
         acc[square.id] = square;
         return acc;
       },
-      {} as Record<string, SquareState>
+      {} as Record<string, Square>
     );
 }
 
-type Point = {
-  x: number;
-  y: number;
-  z: number;
-};
-function threePointBezier(p1: Point, p2: Point, p3: Point, t: number) {
-   
-  return {
-    x: bezier(p1.x, p2.x, p3.x, t),
-    y: bezier(p1.y, p2.y, p3.y, t),
-    z: bezier(p1.z, p2.z, p3.z, t)
-  };
+function resetSquare(old: Square): Square {
+  old.initial.x = Math.floor(Math.random() * 30);
+  old.initial.y = Math.floor(Math.random() * 30); // random starting Y position
+  old.initial.z = Math.random() * 5;
+  old.progressRate = Math.random();
+  old.x = 0;
+  old.y = 0;
+  old.z = 0;
+  old.opacity = 0;
+  old.progress = 0;
+
+  return old;
 }
+
 function bezier(p1: number, p2: number, p3: number, t: number) {
-  return (((1 - t) ** 2) * p1) + (2 * (1 - t) * t * p2) + ((t ** 2) * p3);
+  return (1 - t) ** 2 * p1 + 2 * (1 - t) * t * p2 + t ** 2 * p3;
 }
