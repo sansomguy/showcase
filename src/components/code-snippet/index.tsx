55d758468ff115940783f36584f137907ec7d7df
Josh Sansom-Sherwill <joshwebd@gmail.com>
2024-08-18 16:25:56 +1000

    Moved back to express and trying to deploy with Netlify. I don't care enough about latency to be wasting time with vercel and cloudflare at this point

--- src/components/code-snippet/index.tsx
+++ src/components/code-snippet/index.tsx
@@ -1,39 +1,28 @@
 import { component$, Resource, useResource$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
-import { createHighlighterCore, loadWasm } from "shiki/core";
-import theme from "shiki/dist/themes/dark-plus.mjs";
-import js from "shiki/langs/js.mjs";
+import { server$ } from "@builder.io/qwik-city";
+import { codeToHtml } from "shiki";
+import theme from "shiki/themes/dark-plus.mjs";
 
-type Language = "javascript";
+type Language = "javascript" | "typescript" | "html" | "python";
 
 export type ServerRenderProps = {
   language: Language;
   code: string;
 };
+export const renderCode = server$(async function (props: ServerRenderProps) {
+  return await codeToHtml(props.code, {
+    lang: props.language,
+    theme,
+  });
+});
 export default component$(({ language, code }: ServerRenderProps) => {
-  const location = useLocation();
   const styledCode = useResource$(async ({ cache, track }) => {
     cache("immutable");
     track(() => language);
     track(() => code);
-
-    async function loadDependencies() {
-      const origin = new URL(location.url).origin;
-      await fetch(`${origin}/shiki/onig.wasm`)
-        .then((res) => res.arrayBuffer())
-        .then((x) => loadWasm(x));
-    }
-
-    await loadDependencies();
-
-    const highlighter = await createHighlighterCore({
-      themes: [theme],
-      langs: [js],
-    });
-
-    return highlighter.codeToHtml(code, {
+    return await codeToHtml(code, {
       lang: language,
-      theme: "dark-plus",
+      theme,
     });
   });
   return (
