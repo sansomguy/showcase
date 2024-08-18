6233f227079e44b8c3e857fd29e834fc6af3caa0
Josh Sansom-Sherwill <joshwebd@gmail.com>
2024-08-18 13:56:57 +1000

    Got the code-snippet stuff loading with ssr, but I'm thinking about how to do this in the future so that we don't have to worry about the size of js on the server in the future. I'm really want to just self host at this point.

--- src/components/code-snippet/index.tsx
+++ src/components/code-snippet/index.tsx
@@ -1,18 +1,37 @@
 import { component$, Resource, useResource$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
+import { createHighlighterCore, loadWasm } from "shiki/core";
+import theme from "shiki/dist/themes/dark-plus.mjs";
+import js from "shiki/langs/js.mjs";
 
-type Language = "javascript" | "python" | "html" | "css";
+type Language = "javascript";
 
 export type ServerRenderProps = {
   language: Language;
   code: string;
 };
 export default component$(({ language, code }: ServerRenderProps) => {
-  const styledCode = useResource$(async ({ cache }) => {
+  const location = useLocation();
+  const styledCode = useResource$(async ({ cache, track }) => {
     cache("immutable");
-    const {
-      default: { codeToHtml },
-    } = await import("shiki");
-    return await codeToHtml(code, {
+    track(() => language);
+    track(() => code);
+
+    async function loadDependencies() {
+      const origin = new URL(location.url).origin;
+      await fetch(`${origin}/shiki/onig.wasm`)
+        .then((res) => res.arrayBuffer())
+        .then((x) => loadWasm(x));
+    }
+
+    await loadDependencies();
+
+    const highlighter = await createHighlighterCore({
+      themes: [theme],
+      langs: [js],
+    });
+
+    return highlighter.codeToHtml(code, {
       lang: language,
       theme: "dark-plus",
     });
